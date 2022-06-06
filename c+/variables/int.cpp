88115154b9fc59be73f164e7a90e1d24848ff528
OmarMWarraich <earthling986@gmail.com>
2022-06-06 22:09:43 +0500

    feat: Types of variables

--- c+/variables/int.cpp
+++ c+/variables/int.cpp
@@ -4,16 +4,55 @@ using namespace std;
 
 main ()
 {
-    int A = 4;
+    int a = 40, b, c =20;    // 4 bytes, -2 billions to 2 billions
 
-    cout << A << endl;
+    cout << "a : " << a << " address: " << &a << endl;
+    cout << "b : " << b << " address: " << &b << endl;
+    cout << "c : " << c << " address: " << &c << endl;
 
-    cout << &A << endl;
+    short t1 = 5;  // -32768 to 32767, 2 bytes
 
-    A = 10;
+    cout << t1 << endl;
 
-    cout << A << endl;
-    cout << &A << endl;
+    float t2 = 5.23;  // 4 bytes that are upto 38 zeros
 
+    cout << t2 << endl;
 
+    double t3 = 5.23;  // 8 bytes that are upto 308 zeros
+
+    cout << t3 << endl;
+
+    char t4;  // characters
+
+    t4 = 'a';
+
+    cout << t4 << endl;
+
+    string t5 = "Hello World :-)";
+
+    cout << t5 << endl;
+
+    string x = "part1";
+
+    string y = " part2";
+
+    string combined_Strings = x + y;
+
+    cout << combined_Strings << endl;
+
+    // boolean | true or false, false is always zero, every other number is true.
+
+    bool t6 = true;
+
+    cout << t6 << endl;
+
+    // for unsigned short int, 0 to 65535;
+
+    unsigned short t7 = 65000;
+
+    cout << t7 << endl;
+
+    const string NAMEOFGAME = "conqueror of rome";   // immutable string
+
+    cout << NAMEOFGAME << endl;
 }
