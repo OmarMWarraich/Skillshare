47c2b5add5a5b6169e257896a3877bc15d47b65a
OmarMWarraich <earthling986@gmail.com>
2022-06-06 22:31:22 +0500

    feat: downloading data from input

---
+++ c+/input/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+main () 
+{
+    string name, surname;
+
+    cout << "Enter your name: ";
+
+    cin >> name;
+
+    cout << "Enter your surname: ";
+
+    cin >> surname;
+
+    cout << "Welcome " << name << " " << surname;
+}
