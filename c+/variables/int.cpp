2db65d83b3c1572889616e8613b9597e5e1eadc7
OmarMWarraich <earthling986@gmail.com>
2022-06-06 14:09:15 +0500

    feat: Initialization of Variables
    allocation of variables

---
+++ c+/variables/int.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main ()
+{
+    int A = 4;
+
+    cout << A << endl;
+
+    cout << &A << endl;
+
+    A = 10;
+
+    cout << A << endl;
+    cout << &A << endl;
+
+
+}
