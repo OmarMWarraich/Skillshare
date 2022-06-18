1036ebb69f275a7317d7e2d8a573f117feeaa6c9
OmarMWarraich <earthling986@gmail.com>
2022-06-18 06:22:08 +0500

    feat: displayed list on the browser
    1. Described the interface AllUsersInt.
    2. Type annotated useState as AllUsersInt and added allUsersInt array.
    3. Defined submitForm as an event trigerred as React.SyntheticEvent of type void.
    4. setUsersState of the currentUser in the array of allUsers as that of the currentUser.
    5. Mapped allUsers to each user's props.
    6. Attributed submitForm to form onSubmit.
    7. Printed allUsers.

--- user-list-typescript/src/App.tsx
+++ user-list-typescript/src/App.tsx
@@ -2,22 +2,30 @@ import React, {useState} from "react";
 import './App.css';
 
 const App: React.FC = () => {
+
   interface UserInt {
     name: string,
     age: string,
     job: string
   }
 
-const [usersState, setUsersState] = useState<{ currentUser: UserInt }> ({
+  interface AllUsersInt {
+    currentUser: UserInt;
+    allUsers: Array<UserInt>;
+  }
+
+const [usersState, setUsersState] = useState<AllUsersInt> ({
       currentUser: {
       name: "",
       age: "",
       job:""
-    }
+    },
+    allUsers: []
 })
 
 const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) : void => {
   setUsersState({
+    ...usersState,
     currentUser: {
       ...usersState.currentUser,
       [e.target.name] : e.target.value      
@@ -25,12 +33,36 @@ const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) : void => {
   })
 }
 
-console.log(usersState.currentUser);
+const submitForm = (e: React.SyntheticEvent) : void=> {
+  e.preventDefault();
+
+  setUsersState({
+    currentUser: {
+      name: "",
+      age: "",
+      job: ""
+    },
+    allUsers: [
+      ...usersState.allUsers,
+      usersState.currentUser
+    ]
+  })
+}
+
+const allUsers = usersState.allUsers.map((user, i) => (
+  <div key={i}>
+    <h2>{user.name}</h2>
+    <h2>{user.age}</h2>
+    <h2>{user.job}</h2>
+  </div>
+))
+
+console.log(usersState);
 
   return (
     <div className="Container">
       <h1>React with TypeScript</h1>
-      <form>
+      <form onSubmit={submitForm}>
         <label htmlFor="userName">Name:</label>
         <input
           id="userName"
@@ -60,6 +92,7 @@ console.log(usersState.currentUser);
 
         <button type="submit">Add User</button>
       </form>
+      { allUsers }
     </div>
   );
 }
