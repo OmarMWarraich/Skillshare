3d1297699ce77cc56c21fedf00cc6c186dced12c
OmarMWarraich <earthling986@gmail.com>
2022-06-25 03:31:45 +0500

    feat: added the Navbar
    Navbar
    1. Removed the redundant dirs.
    2. dependency: '@mui/material @emotion/react @emotion/styled'
    3. imported: Button.
    4. added: buttons.

--- Cryptocurrency_ICO_page/ico_page/src/App.js
+++ Cryptocurrency_ICO_page/ico_page/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav>
+        <a href="/" class="titleICO">
+          <div>DEMOICO</div>
         </a>
-      </header>
+        <div class="nav-links">
+          <a id="about" href="/about"><Button>About</Button></a>
+          <a id="whitepaper" href="/whitepaper"><Button>Whitepaper</Button></a>
+          <a id="roadmap" href="/roadmap"><Button>Roadmap</Button></a>
+          <a id="team" href="/team"><Button>Team</Button></a>
+        </div> 
+        
+        <div class="nav-address">
+          <div class="address"><Button variant="text">Adress: "0x0"</Button></div>
+          <div class="eth"><Button>ETH: 12</Button></div>
+          <div class="dctoken"><Button>My DC: 1223</Button></div>
+        </div>
+
+      </nav>
     </div>
   );
 }
