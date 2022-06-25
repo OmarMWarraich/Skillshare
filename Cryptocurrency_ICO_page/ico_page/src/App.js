08d15cb46f83114ecf46e6e573149011fe83f5be
OmarMWarraich <earthling986@gmail.com>
2022-06-25 19:15:59 +0500

    feat: added scroll-to-component
    fix: navbar overlay

--- Cryptocurrency_ICO_page/ico_page/src/App.js
+++ Cryptocurrency_ICO_page/ico_page/src/App.js
@@ -7,22 +7,28 @@ import Whitepaper from './components/Whitepaper';
 import Roadmap from './components/Roadmap';
 import Contribute from './components/Contribute';
 import Team from './components/Team';
+import scrollToComponent from 'react-scroll-to-component';
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div>
         <nav>
+
           <a href="/" class="titleICO">
             <i class="material-icons">group_work</i>
             <div>DEMOICO</div>
           </a>
 
           <div class="middleNav">
-            <a id="about"><Button>About</Button></a>
-            <a id="whitepaper"><Button>Whitepaper</Button></a>
-            <a id="roadmap"><Button>Roadmap</Button></a>
-            <a id="team"><Button>Team</Button></a>
+            <a onClick={() => scrollToComponent(this.About, { offset: -70, align: 'top', duration: 1500})}><Button>About</Button></a>
+            <a onClick={() => scrollToComponent(this.Whitepaper, { offset: -70, align: 'top', duration: 1500})}><Button>Whitepaper</Button></a>
+            <a onClick={() => scrollToComponent(this.Roadmap, { offset: -70, align: 'top', duration: 1500})}><Button>Roadmap</Button></a>
+            <a onClick={() => scrollToComponent(this.Contribute, { offset: -70, align: 'top', duration: 1500})}><Button>Contribute</Button></a>
+            <a onClick={() => scrollToComponent(this.Team, { offset: -70, align: 'top', duration: 1500})}><Button>Team</Button></a>
           </div>
 
           <div class="rightNav">
@@ -33,17 +39,17 @@ class App extends Component {
               <div class="eth"> ETH: 12 </div>
               <div class="dctoken"> My DC: 1223 </div>
             </div>
-
           </div>
 
         </nav>
 
-        <div id="startDiv"><Start /></div>
-        <div id="aboutDiv"><About /></div>
-        <div id="whitepaperDiv"><Whitepaper /></div>
-        <div id="roadmapDiv"><Roadmap /></div>
-        <div id="contributeDiv"><Contribute /></div>
-        <div id="teamDiv"><Team /></div>
+        <div id="startDiv"> <Start/> </div>
+        <div ref={(section) => { this.About = section; }}><About/></div>
+        <div ref={(section) => { this.Whitepaper = section; }}> <Whitepaper/> </div>
+        <div ref={(section) => { this.Roadmap = section; }}> <Roadmap/> </div>
+        <div ref={(section) => { this.Contribute = section; }}> <Contribute/> </div>
+        <div ref={(section) => { this.Team = section; }}> <Team/> </div>
+
       </div>
     );
   }
