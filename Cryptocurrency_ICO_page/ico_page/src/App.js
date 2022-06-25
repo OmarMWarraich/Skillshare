edbf6f80e143576092f217a2782361a5b092ea5d
OmarMWarraich <earthling986@gmail.com>
2022-06-25 15:23:09 +0500

    feat: added and styled the Start component

--- Cryptocurrency_ICO_page/ico_page/src/App.js
+++ Cryptocurrency_ICO_page/ico_page/src/App.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/App.css';
 import Button from '@mui/material/Button';
+import Start from './components/Start';
 
-function App() {
-  return (
-    <div>
-      <nav>
-        <a href="/" class="titleICO">
-          <div>DEMOICO</div>
-        </a>
-        <div class="nav-links">
-          <a id="about" href="/about"><Button>About</Button></a>
-          <a id="whitepaper" href="/whitepaper"><Button>Whitepaper</Button></a>
-          <a id="roadmap" href="/roadmap"><Button>Roadmap</Button></a>
-          <a id="team" href="/team"><Button>Team</Button></a>
-        </div> 
-        
-        <div class="nav-address">
-          <div class="address"><Button variant="text">Adress: "0x0"</Button></div>
-          <div class="eth"><Button>ETH: 12</Button></div>
-          <div class="dctoken"><Button>My DC: 1223</Button></div>
-        </div>
 
-      </nav>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <a href="/" class="titleICO">
+            <i class="material-icons">group_work</i>
+            <div>DEMOICO</div>
+          </a>
+
+          <div class="middleNav">
+            <a id="about"><Button>About</Button></a>
+            <a id="whitepaper"><Button>Whitepaper</Button></a>
+            <a id="roadmap"><Button>Roadmap</Button></a>
+            <a id="team"><Button>Team</Button></a>
+          </div>
+
+          <div class="rightNav">
+            <i class="material-icons">account_box</i>
+
+            <div class="myAccountBox">
+              <div class="address"> Address: 0x47d8e19Ab54CED9d4Afc7C5519C6481C3D893d8d </div>
+              <div class="eth"> ETH: 12 </div>
+              <div class="dctoken"> My DC: 1223 </div>
+            </div>
+
+          </div>
+
+        </nav>
+
+        <div id="startDiv"><Start /></div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ Cryptocurrency_ICO_page/ico_page/src/components/Start.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import Button from '@mui/material/Button';
+import { Player, BigPlayButton, ControlBar } from 'video-react';
+
+class Start extends Component {
+
+  render() {
+    return (
+      <div>
+        <div class="container">
+          <div class="containerMiddle">
+            <div class="header1">#ICO Landing page for your cryptocurrency project</div>
+
+            <div class="flex">
+              <div class="textArea">
+                <div>Decentralized Demo Platform for ICO Developers, Advisors, Crypto-Experts
+                     and Investors. </div>
+                <div class="buttonContainer">
+                  <div> <Button variant="contained" color="primary"> SIGN UP TO JOIN </Button> </div>
+                </div>
+              </div>
+
+              <div class="movieContainer">
+                <Player poster="https://blog.sodio.tech/wp-content/uploads/2018/03/ethex-is-decentralized2x.1551cb1c.png" src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" >
+                  <BigPlayButton position="center" />
+                   <ControlBar autoHide={false} disableCompletely={true} />
+                </Player>
+              </div>
+            </div>
+
+            <div class="tokenSaleContainer flex">
+              <div class="tokenSaleLeftSide">
+                <div class="flex space-between">
+                  <div class="tokensSold">Tokens Sold: 20</div>
+                  <div class="contributors">Contributers:<b> 50</b></div>
+                </div>
+                <div class="totalSuppy">10 <b>DC</b></div>
+                <div id="contribute" href=""><Button variant="contained" color="primary" >BUY TOKENS | 25% Bonus</Button></div>
+              </div>
+
+              <div class="tokenSaleRightSide">
+                <div class="titleTokenSale">TOKEN SALE IS LIVE</div>
+                <div class="tokenSaleEnds"> TOKEN SALE ENDs IN </div>
+                <div class="time flex space-around">
+                  <div>
+                    <div class="headerTime">23</div>
+                    <div>Days</div>
+                  </div>
+                  <div>
+                    <div class="headerTime" >12</div>
+                    <div>Hours</div>
+                  </div>
+                  <div>
+                    <div class="headerTime" >49</div>
+                    <div>Min</div>
+                  </div>
+                  <div>
+                    <div class="headerTime" >2</div>
+                    <div>Sec</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="Icons space-around flex">
+          <i class="fab fa-bitcoin"></i>
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-telegram-plane"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-reddit-alien"></i>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Start;
