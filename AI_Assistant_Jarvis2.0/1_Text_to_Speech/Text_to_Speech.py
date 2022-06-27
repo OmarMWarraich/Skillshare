9b920fb018fc46085061da5c7e3a1d3c5550d803
OmarMWarraich <earthling986@gmail.com>
2022-06-27 22:21:12 +0500

    Initial Commit: AI_Asst_Jarvis2

---
+++ AI_Assistant_Jarvis2.0/1_Text_to_Speech/Text_to_Speech.py
@@ -0,0 +1,7 @@
+import pyttsx3 as pyt # pip install pyttsx3 == text to speech
+
+engine = pyt.init()
+
+engine.say("hello this is Omar Mohammad Warraich. A naturally stupid person trying his level best to survive in a deep "
+            "fake world by applying artificial intelligence, bare metal programming techniques in the real world, besides, creating decentralized applications, networks, smart contracts, and deploying saas apps, unity, blender games on cloud, IOS platform. ")
+engine.runAndWait()
